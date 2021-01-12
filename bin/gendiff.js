5a3406daf41a7ffba897bf4a80e7a01a4bbed962
nofacez <genizzz@yandex.ru>
2021-01-12 19:08:42 +0300

    small patch

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
-let _ = require('lodash');
-let fs = require('fs');
+const _ = require('lodash');
+const fs = require('fs');
 const path = require('path');
-const { Command, help } = require('commander'); // (normal include)
-const { toPairs } = require('lodash');
+const { Command } = require('commander'); // (normal include)
 
-const getPath = (dir, filename) => path.resolve(dir, filename)
+const getPath = (dir, filename) => path.resolve(dir, filename);
 
-const gendiff = (filepath1, filepath2) => {
-  const dir = process.cwd()
+const gendiff = () => {
   const program = new Command();
   program
     .version('0.0.1')
@@ -16,7 +14,7 @@ const gendiff = (filepath1, filepath2) => {
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
 
-    .action(function(f1, f2) {
+    .action((f1, f2) => {
       const resultArray = [];
       const dir = process.cwd();
       const file1 = JSON.parse(fs.readFileSync(getPath(dir, f1)));
@@ -33,21 +31,21 @@ const gendiff = (filepath1, filepath2) => {
           } else {
             resultArray.push(['-', key, file1[key]]);
             resultArray.push(['+', key, file2[key]]);
-        };
-      };
-      if (_.has(file1, key) && !_.has(file2, key)) {
-        resultArray.push(['-', key, file1[key]]);
-      }
-      if (!_.has(file1, key) && _.has(file2, key)) {
-        resultArray.push(['+', key, file2[key]]);
-      }
-    })
-    const fin = resultArray.map(([status, key, value]) => `\t${status} ${key}: ${value}`);
+          }
+        }
+        if (_.has(file1, key) && !_.has(file2, key)) {
+          resultArray.push(['-', key, file1[key]]);
+        }
+        if (!_.has(file1, key) && _.has(file2, key)) {
+          resultArray.push(['+', key, file2[key]]);
+        }
+      });
+      const fin = resultArray.map(([status, key, value]) => `\t${status} ${key}: ${value}`);
 
-    console.log(`{\n${fin.join('\n')}\n}`);
+      console.log(`{\n${fin.join('\n')}\n}`);
     });
 
-program.parse(process.argv);
-}
+  program.parse(process.argv);
+};
 
-gendiff()
+gendiff();
