ac69a6ef880757eb506650232f8c5139bc890876
nofacez <genizzz@yandex.ru>
2021-01-14 19:58:32 +0300

    fix

--- src/testfile.js
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-
-const getFixturePath = (filename, format = '') => console.log(path.normalize(path.join('__fixtures__', format, filename)));
-
-getFixturePath('file1.json', 'json');
