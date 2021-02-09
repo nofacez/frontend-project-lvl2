c1ed258cab6b315f0bde4b65ca3bbbd699c854be
nofacez <genizzz@yandex.ru>
2021-02-09 17:58:29 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,27 +4,13 @@ import finddiff from '../src/finddiff.js';
 
 const program = new Command();
 
-function errorColor(str) {
-  // Add ANSI escape codes to display text in red.
-  return `\x1b[31m${str}\x1b[0m`;
-}
-
 program
-  .configureOutput({
-    // Visibly override write routines as example!
-    writeOut: (str) => process.stdout.write(`[OUT] ${str}`),
-    writeErr: (str) => process.stdout.write(`[ERR] ${str}`),
-    // Output errors in red.
-    outputError: (str, write) => write(errorColor(str)),
-  });
-
-program
-  .version('0.0.1')
-  .arguments('<filepath1>, <filepath2>')
+  .version('0.0.2')
+  .arguments('<filepath1, <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((file1, file2) => console.log(finddiff(file1, file2, program.format)));
 
-program.parse(process.argv);
+program.parse();
 
 // export default gendiff;
