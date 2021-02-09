b852838fdc9b6d9491bd02e97f200ce09547e634
nofacez <genizzz@yandex.ru>
2021-02-09 18:28:03 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,13 +5,13 @@ import finddiff from '../src/finddiff.js';
 const program = new Command();
 
 program
-  .version('0.0.2')
+  .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .allowUnknownOption()
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((file1, file2) => console.log(finddiff(file1, file2, program.format)));
+  .action((file1, file2) => console.log(finddiff(file1, file2, program.format), process.argv));
 
-program.parse(process.argv);
+program.parse();
 
 // export default gendiff;
