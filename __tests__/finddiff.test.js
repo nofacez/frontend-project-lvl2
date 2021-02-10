8577699fe5782023a0f057706139fc5b809fedee
nofacez <genizzz@yandex.ru>
2021-02-10 15:52:33 +0300

    fix

--- __tests__/finddiff.test.js
+++ __tests__/finddiff.test.js
@@ -1,11 +1,17 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import gendiff from '../src/finddiff';
 
-const getFixturePath = (filename) => path.normalize(path.join(process.cwd(), '__fixtures__', filename));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// const getFixturePath = (filename) => path.normalize(path.join(, '__fixtures__', filename));
+const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
 
 test('gendiff deep stylish', () => {
+  console.log(getFixturePath('file1.json'));
   const dif = gendiff(getFixturePath('file1.json'), getFixturePath('file2.yml'));
   const result = fs.readFileSync(getFixturePath('stylishResult'), 'utf-8');
   expect(dif).toEqual(result);
