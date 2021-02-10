882bc86468004b44318e6435f11f58a63b44f0b4
nofacez <genizzz@yandex.ru>
2021-02-10 15:12:23 +0300

    fix

--- __tests__/finddiff.test.js
+++ __tests__/finddiff.test.js
@@ -7,18 +7,18 @@ const getFixturePath = (filename) => path.normalize(path.join(process.cwd(), '__
 
 test('gendiff deep stylish', () => {
   const dif = gendiff(getFixturePath('file1.json'), getFixturePath('file2.yml'));
-  const result = fs.readFileSync(getFixturePath('stylishResult.txt'), 'utf-8');
+  const result = fs.readFileSync(getFixturePath('stylishResult'), 'utf-8');
   expect(dif).toEqual(result);
 });
 
 test('plain formater', () => {
   const dif = gendiff(getFixturePath('file1.json'), getFixturePath('file2.yml'), 'plain');
-  const result = fs.readFileSync(getFixturePath('plainResult.txt'), 'utf-8');
+  const result = fs.readFileSync(getFixturePath('plainResult'), 'utf-8');
   expect(dif).toEqual(result);
 });
 
 test('toJson', () => {
   const dif = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
-  const result = fs.readFileSync(getFixturePath('result4.txt'), 'utf-8');
+  const result = fs.readFileSync(getFixturePath('result4'), 'utf-8');
   expect(dif).toEqual(result);
 });
